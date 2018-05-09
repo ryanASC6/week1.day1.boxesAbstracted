1ec675f15f3a1df2a01d76bebccda515700593d4
mahdiASC <mahdi@allstarcode.org>
2018-05-09 17:36:42 -0400

    finished bonus

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function draw(){
         const x = i*box_width;
         for(let j = 0;j < number_of_boxes_in_column; j++){
             const y = j*box_height;
-            drawBoxes();
+            drawBox(x,y);
         }
     }
 }
@@ -30,11 +30,36 @@ const customFill = function(i,y){
     fill(255/number_of_boxes_in_row * (i+1), 100, 255/number_of_boxes_in_column * (j+1) );
 }
 
-const drawBoxes = function(){
-    customFill(i,y);
-    rect(x,y,box_width,box_height);
+
+// Bonus - mouse clicks change box color
+// NOTE: there are other ways of implementing this!
+let mouseClicks = [];
+function mousePressed(){
+    mouseClicks.push([mouseX,mouseY]);
 }
 
-function mousePressed(){
+const drawBox = function(x,y){
+    let foundClicks = mouseClicks.filter(mouseClickCord=>{
+        const mX = mouseClickCord[0];
+        const mY = mouseClickCord[1];
+        
+        // mouseclick was within box
+        return mX > x && mX < x + box_width && mY > y && mY < y + box_height;
+    });
 
+    // if one mouse click found, make red
+    if(foundClicks.length === 1){
+        fill("red");
+    }else{
+        // if two mouse clicks found, remove both
+        // and let normal color stay
+        if(foundClicks.length === 2){
+            foundClicks.forEach(mouseClick=>{
+                mouseClicks.splice(mouseClicks.indexOf(mouseClick),1);
+            });
+        }
+        customFill(x/box_width, y);
+    }
+
+    rect(x,y,box_width,box_height);
 }
